a86642afc63c62d93860a9c6b0a67ccbe724e706
adikrovi <jrkrovi@gmail.com>
2023-05-25 11:26:34 -0400

    fixes

--- .d/main.cpp.d
+++ .d/main.cpp.d
@@ -29,10 +29,13 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/display/lv_themes/lv_theme_mono.h \
  include/display/lv_themes/lv_theme_nemo.h \
  include/display/lv_themes/lv_theme_material.h \
- include/display/lv_objx/lv_imgbtn.h \
- include/display/lv_draw/lv_draw_img.h include/display/lv_draw/lv_draw.h \
- include/display/lv_misc/lv_txt.h include/display/lv_misc/lv_area.h \
- include/display/lv_misc/lv_font.h include/display/lv_draw/lv_draw_rect.h \
+ include/display/lv_objx/lv_btn.h include/display/lv_objx/lv_cont.h \
+ include/display/lv_core/lv_indev.h include/display/lv_hal/lv_hal_indev.h \
+ include/display/lv_core/lv_group.h include/display/lv_objx/lv_imgbtn.h \
+ include/display/lv_objx/lv_btn.h include/display/lv_draw/lv_draw_img.h \
+ include/display/lv_draw/lv_draw.h include/display/lv_misc/lv_txt.h \
+ include/display/lv_misc/lv_area.h include/display/lv_misc/lv_font.h \
+ include/display/lv_draw/lv_draw_rect.h \
  include/display/lv_draw/lv_draw_label.h \
  include/display/lv_draw/lv_draw_img.h \
  include/display/lv_draw/lv_draw_line.h \
@@ -41,18 +44,26 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/display/lv_misc/lv_symbol_def.h \
  include/display/lv_objx/lv_label.h include/display/lv_draw/lv_draw.h \
  include/display/lv_objx/lv_label.h include/display/lv_objx/lv_line.h \
- include/display/lv_objx/lv_page.h include/display/lv_core/lv_indev.h \
- include/display/lv_hal/lv_hal_indev.h include/display/lv_core/lv_group.h \
+ include/display/lv_objx/lv_page.h include/display/lv_objx/lv_cont.h \
  include/display/lv_objx/lv_list.h include/display/lv_objx/lv_page.h \
- include/display/lv_objx/lv_img.h include/display/lv_objx/lv_table.h \
+ include/display/lv_objx/lv_img.h include/display/lv_objx/lv_chart.h \
+ include/display/lv_objx/lv_line.h include/display/lv_objx/lv_table.h \
  include/display/lv_objx/lv_cb.h include/display/lv_objx/lv_bar.h \
  include/display/lv_objx/lv_slider.h include/display/lv_objx/lv_bar.h \
- include/display/lv_objx/lv_kb.h include/display/lv_objx/lv_canvas.h \
- include/display/lv_objx/lv_img.h include/display/lv_objx/lv_tabview.h \
- include/display/lv_objx/lv_page.h include/display/lv_objx/lv_mbox.h \
+ include/display/lv_objx/lv_led.h include/display/lv_objx/lv_btnm.h \
+ include/display/lv_objx/lv_kb.h include/display/lv_objx/lv_btnm.h \
+ include/display/lv_objx/lv_ddlist.h include/display/lv_objx/lv_page.h \
+ include/display/lv_objx/lv_label.h include/display/lv_objx/lv_roller.h \
+ include/display/lv_objx/lv_ddlist.h include/display/lv_objx/lv_ta.h \
+ include/display/lv_objx/lv_canvas.h include/display/lv_objx/lv_img.h \
+ include/display/lv_objx/lv_win.h include/display/lv_objx/lv_tabview.h \
+ include/display/lv_objx/lv_win.h include/display/lv_objx/lv_tileview.h \
+ include/display/lv_objx/lv_mbox.h include/display/lv_objx/lv_gauge.h \
+ include/display/lv_objx/lv_lmeter.h include/display/lv_objx/lv_lmeter.h \
  include/display/lv_objx/lv_sw.h include/display/lv_objx/lv_slider.h \
  include/display/lv_objx/lv_arc.h include/display/lv_objx/lv_preload.h \
  include/display/lv_objx/lv_arc.h include/display/lv_objx/lv_calendar.h \
+ include/display/lv_objx/lv_spinbox.h include/display/lv_objx/lv_ta.h \
  include/pros/misc.h include/pros/motors.h include/pros/optical.h \
  include/pros/error.h include/pros/rtos.h include/pros/rotation.h \
  include/pros/screen.h include/pros/colors.h include/pros/vision.h \
@@ -124,7 +135,13 @@ include/display/lv_themes/lv_theme_zen.h:
 include/display/lv_themes/lv_theme_mono.h:
 include/display/lv_themes/lv_theme_nemo.h:
 include/display/lv_themes/lv_theme_material.h:
+include/display/lv_objx/lv_btn.h:
+include/display/lv_objx/lv_cont.h:
+include/display/lv_core/lv_indev.h:
+include/display/lv_hal/lv_hal_indev.h:
+include/display/lv_core/lv_group.h:
 include/display/lv_objx/lv_imgbtn.h:
+include/display/lv_objx/lv_btn.h:
 include/display/lv_draw/lv_draw_img.h:
 include/display/lv_draw/lv_draw.h:
 include/display/lv_misc/lv_txt.h:
@@ -143,29 +160,45 @@ include/display/lv_draw/lv_draw.h:
 include/display/lv_objx/lv_label.h:
 include/display/lv_objx/lv_line.h:
 include/display/lv_objx/lv_page.h:
-include/display/lv_core/lv_indev.h:
-include/display/lv_hal/lv_hal_indev.h:
-include/display/lv_core/lv_group.h:
+include/display/lv_objx/lv_cont.h:
 include/display/lv_objx/lv_list.h:
 include/display/lv_objx/lv_page.h:
 include/display/lv_objx/lv_img.h:
+include/display/lv_objx/lv_chart.h:
+include/display/lv_objx/lv_line.h:
 include/display/lv_objx/lv_table.h:
 include/display/lv_objx/lv_cb.h:
 include/display/lv_objx/lv_bar.h:
 include/display/lv_objx/lv_slider.h:
 include/display/lv_objx/lv_bar.h:
+include/display/lv_objx/lv_led.h:
+include/display/lv_objx/lv_btnm.h:
 include/display/lv_objx/lv_kb.h:
+include/display/lv_objx/lv_btnm.h:
+include/display/lv_objx/lv_ddlist.h:
+include/display/lv_objx/lv_page.h:
+include/display/lv_objx/lv_label.h:
+include/display/lv_objx/lv_roller.h:
+include/display/lv_objx/lv_ddlist.h:
+include/display/lv_objx/lv_ta.h:
 include/display/lv_objx/lv_canvas.h:
 include/display/lv_objx/lv_img.h:
+include/display/lv_objx/lv_win.h:
 include/display/lv_objx/lv_tabview.h:
-include/display/lv_objx/lv_page.h:
+include/display/lv_objx/lv_win.h:
+include/display/lv_objx/lv_tileview.h:
 include/display/lv_objx/lv_mbox.h:
+include/display/lv_objx/lv_gauge.h:
+include/display/lv_objx/lv_lmeter.h:
+include/display/lv_objx/lv_lmeter.h:
 include/display/lv_objx/lv_sw.h:
 include/display/lv_objx/lv_slider.h:
 include/display/lv_objx/lv_arc.h:
 include/display/lv_objx/lv_preload.h:
 include/display/lv_objx/lv_arc.h:
 include/display/lv_objx/lv_calendar.h:
+include/display/lv_objx/lv_spinbox.h:
+include/display/lv_objx/lv_ta.h:
 include/pros/misc.h:
 include/pros/motors.h:
 include/pros/optical.h:
